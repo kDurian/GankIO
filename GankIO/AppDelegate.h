07937683e981c59e8a19f201f74635faa08b9f49
Durian <ljt920108@gmail.com>
2016-11-29 04:13:23 +0800

    create GankIO project

---
+++ GankIO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GankIO
+//
+//  Created by Durian on 2016/11/29.
+//  Copyright © 2016年 durian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GankIO/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GankIO
+//
+//  Created by Durian on 2016/11/29.
+//  Copyright © 2016年 durian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
